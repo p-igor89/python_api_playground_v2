b7cc82fe695701744c8f648215463ed7a637ba61
Peretiatko Ihor <p_ihor@hotmail.com>
2023-07-29 23:12:07 -0400

    add user_details TCs

--- src/assertions.py
+++ src/assertions.py
@@ -25,6 +25,25 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
     @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_no_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it's present."
+
+    @staticmethod
     def assert_status_code(response, expected_status_code):
         assert response.status_code == expected_status_code, \
             f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
