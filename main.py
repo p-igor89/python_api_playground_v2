6871744d297726e0172708b26b2f0bace2562d62
Peretiatko Ihor <p_ihor@hotmail.com>
2023-07-29 22:16:56 -0400

    refactor assertions.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-
-print(response.text)
---
+++ src/assertions.py
@@ -0,0 +1,14 @@
+import json
+
+
+class Assertions:
+    @staticmethod
+    def assert_json_value_by_name(response, name, expected_value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+        assert response_as_dict[name] == expected_value, error_message
