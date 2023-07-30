5445a50e418534bf9023aabb2f091f7adf2ddc60
Peretiatko Ihor <p_ihor@hotmail.com>
2023-07-29 23:57:30 -0400

    refactor all the rest TCs with custom MyRequest wrapper

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,12 +1,11 @@
-import requests
-
 from src.assertions import Assertions
 from src.base_case import BaseCase
+from src.my_requests import MyRequests
 
 
 class TestUserGet(BaseCase):
     def test_user_details_not_auth(self):
-        response = requests.get("https://playground.learnqa.ru/api/user/2")
+        response = MyRequests.get("/user/2")
 
         Assertions.assert_json_has_key(response, "username")
         Assertions.assert_json_has_no_key(response, "email")
@@ -19,14 +18,14 @@ class TestUserGet(BaseCase):
             'password': '1234'
         }
 
-        resource1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        resource1 = MyRequests.post("/user/login", data=data)
 
         auth_sid = self.get_cookie(resource1, "auth_sid")
         token = self.get_header(resource1, "x-csrf-token")
         user_id_from_auth_method = self.get_json_value(resource1, "user_id")
 
-        response2 = requests.get(
-            f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}",
+        response2 = MyRequests.get(
+            f"/user/{user_id_from_auth_method}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid}
         )
