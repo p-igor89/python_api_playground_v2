e556a6be5b369cc334ad1d3e9e7b9c0296e7f6d5
Peretiatko Ihor <p_ihor@hotmail.com>
2023-07-29 22:44:46 -0400

    add some assertions

--- src/assertions.py
+++ src/assertions.py
@@ -1,9 +1,11 @@
 import json
 
+from requests import Response
+
 
 class Assertions:
     @staticmethod
-    def assert_json_value_by_name(response, name, expected_value, error_message):
+    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
@@ -12,3 +14,17 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_status_code(response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,34 @@
+from datetime import datetime
+
 import requests
 
+from src.assertions import Assertions
 from src.base_case import BaseCase
 
 
 class TestUserRegister(BaseCase):
+
+    def setup(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_status_code(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = {
@@ -16,6 +41,7 @@ class TestUserRegister(BaseCase):
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        Assertions.assert_status_code(response, 400)
+
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
             f"Unexpected content {response.content}"
