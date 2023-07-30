0e9f5cf0f0d4b1bafb7de26d69d862e51754f586
Peretiatko Ihor <p_ihor@hotmail.com>
2023-07-29 21:02:58 -0400

    add pytest TC

---
+++ test_examples.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class TestExamples:
+    def test_hello_call(self):
+        name = 'User'
+        data = {'name': name}
+
+        response = requests.get("https://playground.learnqa.ru/api/hello", params=data)
+
+        assert response.status_code == 200, "Wrong response code"
+
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no field 'answer' in the response"
+
+        expected_response_text = f"Hello, {name}"
+        actual_response_text = response_dict["answer"]
+        assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
